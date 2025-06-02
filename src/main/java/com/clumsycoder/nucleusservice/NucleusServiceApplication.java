c4411367ebf2608565e3b9a84856af87ac29be7b
Gaurav Aggarwal <gauravxor@gmail.com>
2025-06-03 01:58:15 +0530

    Initial commit

---
+++ src/main/java/com/clumsycoder/nucleusservice/NucleusServiceApplication.java
@@ -0,0 +1,13 @@
+package com.clumsycoder.nucleusservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NucleusServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NucleusServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/clumsycoder/nucleusservice/NucleusServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clumsycoder.nucleusservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NucleusServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
